5b98bb94b87d3958840186c5e8f9198a5a64cc27
Tetsutaro Maruyama <tetsutaro.maruyama@gmail.com>
2021-01-06 14:20:46 +0900

    add -u option to docker run

--- run_console.sh
+++ run_console.sh
@@ -7,4 +7,4 @@ if [ $1 != '0' ] && [ $1 != '1' ] && [ $1 != '2' ]; then
     echo "indicate proper gpu id"
     exit 1
 fi
-docker run --rm -it --cpus 4.0 --gpus '"device='$1'"' --shm-size 16g --env 'TZ=Asia/Tokyo' --volume ${HOME}/Projects:/home -p 58888:6006 --name ${USER}_console ${USER}:latest /bin/bash
+docker run --rm -it -u $(id -u):$(id -g) --cpus 4.0 --gpus '"device='$1'"' --shm-size 16g --env 'TZ=Asia/Tokyo' --volume ${HOME}/Projects:/home -p 58888:6006 --name ${USER}_console ${USER}:latest /bin/bash
--- run_jupyter.sh
+++ run_jupyter.sh
@@ -7,4 +7,4 @@ if [ $1 != '0' ] && [ $1 != '1' ] && [ $1 != '2' ]; then
     echo "indicate proper gpu id"
     exit 1
 fi
-docker run --detach --cpus 4.0 --gpus '"device='$1'"' --shm-size 16g --env 'TZ=Asia/Tokyo' --volume ${HOME}/Projects:/home -p 58888:8888 --name ${USER}_jupyter ${USER}:latest jupyter lab --ip=0.0.0.0 --allow-root --no-browser --NotebookApp.notebook_dir='/home' --NotebookApp.password=${JUPYTER_PASSWD}
+docker run -u $(id -u):$(id -g) --detach --cpus 4.0 --gpus '"device='$1'"' --shm-size 16g --env 'TZ=Asia/Tokyo' --volume ${HOME}/Projects:/home -p 58888:8888 --name ${USER}_jupyter ${USER}:latest jupyter lab --ip=0.0.0.0 --allow-root --no-browser --NotebookApp.notebook_dir='/home' --NotebookApp.password=${JUPYTER_PASSWD}
